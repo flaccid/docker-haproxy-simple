e4207037b5c785bce9289ad1917a5bd667c99d53
Chris Fordham <chris@fordham-nagy.id.au>
2016-05-02 16:11:06 +1000

    Complete support to reconfigure backend pool.

--- entry.sh
+++ entry.sh
@@ -1,22 +1,34 @@
-# !/bin/bash
+#!/bin/bash
 set -e
 
 # re-configures the haproxy config
 
-# update the listen host and port
+# update the listen host and port, initial backend pool
 sed -i "s/{BIND_HOST}/$BIND_HOST/g" /usr/local/etc/haproxy/haproxy.cfg
 sed -i "s/{BIND_PORT}/$BIND_PORT/g" /usr/local/etc/haproxy/haproxy.cfg
 
-sed -i "s/{BACKEND_POOL}/$BACKEND_POOL/g" /usr/local/etc/haproxy/haproxy.cfg
+echo 'binding:'
+echo "    $BIND_HOST:$BIND_PORT"
+
+# temporarily replace space with hyphon for use with bash array
+BACKEND_HOSTS=${BACKEND_HOSTS// /-}
 
 # backend hosts in backend pool if specified
 if [ ! -z "$BACKEND_HOSTS" ]; then
+  # currently only replacing pool is supported
+  BACKEND_POOL=""
   hosts=(${BACKEND_HOSTS//,/ })
 
+  echo 'backend hosts:'
   for host in "${hosts[@]}"
   do
-    echo "adding backend host ${host}"
+    host=${host//-/ }
+    echo "    ${host}"
+    BACKEND_POOL="$BACKEND_POOL\n        server ${host}"
   done
+
+  # append to the backend pool
+  sed -i "s/{BACKEND_POOL}/$BACKEND_POOL/g" /usr/local/etc/haproxy/haproxy.cfg
 fi
 
 # copied from
